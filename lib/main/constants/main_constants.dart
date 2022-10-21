18ceb4bdf4dc71a0e1d40a96f276391c3c79c3f5
Saud Elabdullah <saudelabdullah@gmail.com>
2022-10-21 19:24:49 +0300

    feat: Added scan feature

--- lib/main/constants/main_constants.dart
+++ lib/main/constants/main_constants.dart
@@ -2,5 +2,6 @@ class MainConstants {
   static List<String> actions = const [
     'json',
     'automation',
+    'file'
   ];
 }
--- lib/main/controllers/main_controller.dart
+++ lib/main/controllers/main_controller.dart
@@ -1,15 +1,19 @@
 import 'dart:io';
 
 import 'package:swiss_dart/automation/controllers/automation_controllers.dart';
+import 'package:swiss_dart/file/controllers/file_controllers.dart';
 import 'package:swiss_dart/json/controllers/json_controller.dart';
 import 'package:swiss_dart/main/constants/main_constants.dart';
 
 class MainController{
   static mainActions(List<String> arguments){
+    print(arguments);
     if(arguments.first.toLowerCase() == MainConstants.actions[0]){
       JsonController.jsonActions(arguments);
     } else if(arguments.first.toLowerCase() == MainConstants.actions[1]){
       AutomationController.automationActions(arguments);
+    } else if(arguments.first.toLowerCase() == MainConstants.actions[2]){
+      FileController.fileActions(arguments);
     } else {
       exitCode = 0;
     }
